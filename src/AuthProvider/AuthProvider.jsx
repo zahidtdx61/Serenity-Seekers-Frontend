9ac575d846e00e4388ed9beeb1111f10b9fec954
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-04-27 20:10:41 +0600

    added google login

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,15 +1,49 @@
-import { createContext } from "react";
+import {
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithPopup,
+} from "firebase/auth";
+import PropTypes from "prop-types";
+import { createContext, useEffect, useState } from "react";
+import auth from "../config/firebase";
 
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const googleProvider = new GoogleAuthProvider();
+  const signInGoogle = () => {
+    setIsLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
   const authData = {
     dummy: "anyTxt",
+    user,
+    setUser,
+    isLoading,
+    setIsLoading,
+    signInGoogle,
   };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log("Observing...");
+      setUser(user);
+      setIsLoading(false);
+    });
+    return () => unsubscribe();
+  }, []);
 
   return (
     <AuthContext.Provider value={authData}>{children}</AuthContext.Provider>
   );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default AuthProvider;
