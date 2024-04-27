25376130abcea6b30ed7c4d2addb2b70a4bfc0b8
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-04-27 20:23:59 +0600

    added log-in page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,4 @@
-import { useContext } from "react";
-import { AuthContext } from "../../AuthProvider/AuthProvider";
-
 const Navbar = () => {
-  const data = useContext(AuthContext);
-  console.log(data);
   return <div>Navbar</div>;
 };
 
