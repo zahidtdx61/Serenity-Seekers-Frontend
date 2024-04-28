0868710523767636767e41c9eaebdff57db68096
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-04-28 13:01:44 +0600

    added Loader

---
+++ src/pages/Loader.jsx
@@ -0,0 +1,13 @@
+import { RingLoader } from "react-spinners";
+
+const Loader = () => {
+  return (
+    <div
+      className={`min-h-[calc(100vh-80px)] flex flex-col  justify-center  items-center `}
+    >
+      <RingLoader size={150} color="#36d7b7" />
+    </div>
+  );
+};
+
+export default Loader;
