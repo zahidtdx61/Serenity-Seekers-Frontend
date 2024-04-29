2dfbc7797d411c657975b084e85792898a0f5273
Md. Zahidur Rahman <45038726+zahidtdx61@users.noreply.github.com>
2024-04-29 09:33:34 +0600

    added not found page

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout/MainLayout";
+import ErrorPage from "../pages/ErrorPage";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
@@ -28,11 +30,19 @@ const router = createBrowserRouter([
       },
       {
         path: "/add-tourist-spot",
-        element: <PrivateRoutes><div>Add Tourists Spot</div></PrivateRoutes>,
+        element: (
+          <PrivateRoutes>
+            <div>Add Tourists Spot</div>
+          </PrivateRoutes>
+        ),
       },
       {
         path: "/my-list",
-        element: <PrivateRoutes><div>My List</div></PrivateRoutes>,
+        element: (
+          <PrivateRoutes>
+            <div>My List</div>
+          </PrivateRoutes>
+        ),
       },
     ],
   },
